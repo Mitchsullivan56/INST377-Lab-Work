60f843a9f26332f610cf1a0f0c8680b81eb095cf
Mitchsullivan56 <66861725+Mitchsullivan56@users.noreply.github.com>
2022-03-13 14:47:10 -0400

    Lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,26 +1,38 @@
 function getRandomIntInclusive(min, max) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
-  return Math.floor(Math.random() * (newMax - newMin) + newMin);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
 }
 
-function dataHandler(dataArray) {
+function restoArrayMake(dataArray) {
   console.log("fired dataHandler");
-  console.table(dataArray);
+  //console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
-    const restNum = getRandomIntInclusive(dataArray);
-    return restNum;
+    const restNum = getRandomIntInclusive(0, dataArray.length);
+    return dataArray[restNum];
   });
-  
-  console.log(listItems);
-  
+
+  //console.log(listItems);
+  return listItems;
   //range.forEach((item) => {
-    //console.log("range item", item);
-    //return[dataArray]
+  //console.log("range item", item);
+  //return[dataArray]
   //});
 }
 
+function createHtmlList(collection) {
+  console.log("fired Html Creator");
+  console.log(collection);
+  const targetList = document.querySelector(".resto-list");
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item; 
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
 async function mainEvent() {
   console.log("script loaded");
   const form = document.querySelector(".main_form");
@@ -37,7 +49,8 @@ async function mainEvent() {
     form.addEventListener("submit", async (submitEvent) => {
       submitEvent.preventDefault();
       console.log("form submission");
-      dataHandler(arrayFromJson.data);
+      const restoArray = restoArrayMake(arrayFromJson);
+      createHtmlList(restoArray);
     });
   }
 }
